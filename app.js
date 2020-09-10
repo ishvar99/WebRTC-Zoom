d504e90f803f98f7765d31d5615c613475c3d182
ishvar99 <ishanvarshney99@gmail.com>
2020-09-11 03:46:31 +0530

    used peer.js for call management

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const express = require("express")
 let app = express()
 const socket = require("socket.io")
 app.set("view engine", "ejs")
-const { v4: uuidv4 } = require("uuid")
 app.use(express.static("public"))
 app.get("/", (req, res) => {
-  res.redirect(`/${uuidv4()}`)
+  res.render("home")
 })
 app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room })
@@ -18,5 +17,8 @@ io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId) => {
     socket.join(roomId)
     socket.to(roomId).broadcast.emit("user-connected", userId)
+    socket.on("disconnect", () => {
+      socket.to(roomId).broadcast.emit("user-disconnected", userId)
+    })
   })
 })
